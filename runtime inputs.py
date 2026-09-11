84147c388e901aab39ad553ea48c294688f030d4
krishnakanthvalluru7-sys <krishnakanthvalluru7@gmail.com>
2026-09-11 15:58:25 +0530

    Add files via upload

---
+++ runtime inputs.py
@@ -0,0 +1,100 @@
+'''a=20
+b=40
+print(a+b)'''
+
+'''a=4
+b=1
+c=a+b
+print(c)'''
+
+#print(3+4)
+
+#run_time input()
+'''a=int(input("enter the a value"))
+b=int(input("enter the b value"))
+print(a+b)'''
+
+'''a=float(input("enter the a value"))
+b=float(input("enter the b value"))
+print(a+b)'''
+
+'''a=str(input("data1"))
+b=str(input("data2"))
+print(a+b)'''
+
+'''a=input("data1")
+b=input("data2")
+print(a+b)'''
+
+'''fname=input("first name")
+lname=input("last name")
+print((fname+" "+lname).title())'''
+
+'''a=complex(input("enter the a value"))
+b=complex(input("enter the b value"))
+print(a+b)'''
+
+'''a=bool(input("enter the a value"))
+b=bool(input("enter the b value"))
+c=bool(input("enter the c value"))
+print(a+b+c)'''
+
+'''a=int(input("enter the a value"))
+b=int(input("enter the b value"))
+option=int(input("choose the option 1.add 2.sub"))
+print(a+b)
+print(a-b)'''
+
+'''a=int(input("enter the a value"))
+b=int(input("enter the b value"))
+option=int(input("choose the option
+1.add
+2.sub"))
+print(a+b)
+print(a-b)'''
+
+'''a=int(input("enter the a value"))
+b=int(input("enter the b value"))
+option=input("choose the option add sub")
+print(a+b)
+print(a-b)'''
+
+'''a=int(input())
+b=int(input())
+print(a+b)'''
+
+'''id_no=int(input("enter idno"))
+name=str(input("enter student name"))
+mobile=int(input("enter mobile no"))
+mailid=input("enter email id")
+clg=input("enter college name")
+branch=input("enter Branch name")
+print("-----------Student Profile---------")
+print("id_no is : " ,id_no)
+print("name is :" ,name)
+print("mobile num is :" ,mobile)
+print("mailid is :" ,mailid)
+print("college is:" ,clg)
+print("branch is:" ,branch)'''
+
+'''a=[10,20,30,40,50]
+a.insert(2,25)'''
+
+'''a=["python","java","c"]
+a.extend(["C","o","d","e"])
+print(a)'''
+
+'''a=("apple","banana","mango")
+c=list(a)
+c.append("kiwi")
+print(tuple(c))'''
+
+'''a={"black","white","red"}
+a.remove("white")
+print(a)'''
+
+a={"name":"pooja","city":"vij"}
+a.update({"state":"ap"})
+print(a)
+
+
